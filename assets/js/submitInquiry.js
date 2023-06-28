12a635cb2c66995c10487044b5fc108ddb506c66
Eric Pineda <ericvpineda@berkeley.edu>
2023-06-28 13:10:00 -0700

    updated -- added submitInquiry.js, implemented basic form submission w 3rd party FORMSUBMIT

---
+++ assets/js/submitInquiry.js
@@ -0,0 +1,20 @@
+const inquiryForm = document.querySelector("#inquiry-form");
+
+inquiryForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const inquiry = e.target.elements;
+
+  fetch("https://formsubmit.co/ajax/ericvpineda@berkeley.edu", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json",
+    },
+    body: JSON.stringify({
+      name: inquiry.name.value,
+      email: inquiry.email.value,
+      message: inquiry.message.value,
+    }),
+  })
+});
