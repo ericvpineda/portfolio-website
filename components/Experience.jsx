1ccec98a4da5e1f5628ccef1b7605331e926390b
Eric Pineda <ericvpineda@berkeley.edu>
2023-12-12 15:03:11 -0800

    updated -- experience component ui

--- components/Experience.jsx
+++ components/Experience.jsx
@@ -25,17 +25,17 @@ export default function Experience() {
   ];
 
   return (
-    <div id="experience" className="section flex-col mb-5 px-10 mt-36 md:mt-0">
+    <div id="experience" className="section flex-col mb-5 px-10 mt-56 sm:mt-0">
       <div className="text-center mb-10">
         <h2 className="section_text">Journey through my</h2>
         <h1 className="header_title">Experience</h1>
       </div>
       <div className="flex w-full gap-4 flex-col sm:flex-row">
         <div className="section_border">
-          <h3 className="font-semibold text-xl tracking-wide mb-5">Frontend</h3>
+          <h3 className="font-semibold text-3xl tracking-wide text-[#495057]">Frontend</h3>
           <ul className="experience_tech">
             {frontendTech.map((tech) => (
-              <li className="whitespace-nowrap">
+              <li className="whitespace-nowrap experience_tech_item">
                 <CodeIcon />
                 {tech}
               </li>
@@ -43,12 +43,12 @@ export default function Experience() {
           </ul>
         </div>
         <div className="section_border">
-          <h3 className="font-semibold text-xl tracking-wide mb-5">
+          <h3 className="font-semibold text-3xl tracking-wide text-[#495057]">
             Backend
           </h3>
           <ul className="experience_tech">
             {backendTech.map((tech) => (
-              <li className="whitespace-nowrap">
+              <li className="whitespace-nowrap experience_tech_item">
                 <CodeOffIcon />
                 {tech}
               </li>
