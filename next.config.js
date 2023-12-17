21d412ef9118bae96e7c04f6e9bdf06c7bcada7a
Eric Pineda <ericvpineda@berkeley.edu>
2023-12-16 16:52:16 -0800

    updated -- next config revert export value

--- next.config.js
+++ next.config.js
@@ -1,20 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-let nextConfig = null;
-
-if (process.env.NODE_ENV === "development") {
-  nextConfig = {
-    images: {
-      domains: ["www.github.com"],
-    },
-  };
-} else {
-  nextConfig = {
-    output: "export",
-    images: {
-      domains: ["www.github.com"],
-    },
-  };
-}
+const nextConfig = {
+  output: "export",
+  images: {
+    domains: ["www.github.com"],
+  },
+};
 
 module.exports = nextConfig;
